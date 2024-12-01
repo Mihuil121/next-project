d58936192c7bef7e470db2748cf77df963403e28
Mihuil121 <bezugliymicha@gmail.com>
2024-12-01 19:48:07 +0300

    Adding media queries for responsive design to improve the display of elements across different screen resolutions.

--- src/app/about/AboutComponent.tsx
+++ src/app/about/AboutComponent.tsx
@@ -5,6 +5,8 @@ import { NextFont } from 'next/dist/compiled/@next/font'
 import { Nunito } from 'next/font/google'
 import photo from '../../Img/backgraund/people.jpeg'
 import Sponsor from "@/components/sponsors/Sponsor"
+import { title } from "process"
+import { Metadata } from "next"
 
 const NunitoA: NextFont = Nunito({
     subsets: ['latin'],
@@ -15,43 +17,48 @@ const NunitoP: NextFont = Nunito({
     weight: '300'
 })
 
-const AboutComponent:React.FC = () => {
-  return (
-    <div>
-         <div>
-            <div className="About">
-                <div className="About-Img-text">
-                    <div className="about-we">
-                        <div className="about-content">
-                            <p className={NunitoA.className}>
-                                We're making a difference
-                                to people's lives
-                            </p>
-                            <div className="content-min">
-                                <p className={NunitoP.className}>
-                                    Creating a way to connect people ready to work, with people <br />
-                                    who need work done
+export const metadata: Metadata = {
+    title: `We're making a difference to people's lives`,
+    description: 'Creating a way to connect people ready to work, with people who need work done'
+}
+
+const AboutComponent: React.FC = () => {
+    return (
+        <div>
+            <div>
+                <div className="About">
+                    <div className="About-Img-text">
+                        <div className="about-we">
+                            <div className="about-content">
+                                <p className={NunitoA.className}>
+                                    We're making a difference
+                                    to people's lives
                                 </p>
+                                <div className="content-min">
+                                    <p className={NunitoP.className}>
+                                        Creating a way to connect people ready to work, with people <br />
+                                        who need work done
+                                    </p>
+                                </div>
                             </div>
                         </div>
+                        <div className="Imgcontent-about">
+                            <Image
+                                src={photo}
+                                alt={`${photo}`}
+                            />
+                        </div>
                     </div>
-                    <div className="Imgcontent-about">
-                        <Image
-                            src={photo}
-                            alt={`${photo}`}
-                        />
+                    <div className="about-flex">
+                        <AboutMision />
                     </div>
-                </div>
-                <div className="about-flex">
-                    <AboutMision />
-                </div>
-                <div className="Sponsor">
-                    <Sponsor />
+                    <div className="Sponsor">
+                        <Sponsor />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default AboutComponent
